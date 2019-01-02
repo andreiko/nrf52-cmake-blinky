0191389fd254bd3241612087ac68194c771e3714
Andrey Bulgakov <mail@andreiko.ru>
2019-01-01 18:21:51 -0800

    Noop logging

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <nrf52.h>
-#include <nrf_delay.h>
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+uint32_t timestamp_func() {
+    return 0;
+}
 
 int main() {
-    NRF_P0->DIRSET = 1 << 17 | 1 << 18 | 1 << 19 | 1 << 20;
+    NRF_P0->DIRSET = 1 << 17;
 
-    while (1) {
-        NRF_P0->OUTSET = 1 << 17 | 1 << 20;
-        NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
-        nrf_delay_ms(300);
+    uint32_t err_code;
+    err_code = NRF_LOG_INIT(timestamp_func);
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
 
-        NRF_P0->OUTSET = 1 << 18 | 1 << 19;
-        NRF_P0->OUTCLR = 1 << 17 | 1 << 20;
-        nrf_delay_ms(300);
+    if (err_code != NRF_SUCCESS) {
+        NRF_P0->DIRSET = 1 << 17 | 1 << 18 | 1 << 19 | 1 << 20;
+        return 1;
     }
+
+    NRF_P0->DIRSET = 1 << 20;
+    return 0;
 }
